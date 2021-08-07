706b59f309319ff90692f7212448f754178ef013
Aravinda <aravinda.sholla@fullthrottlelabs.com>
2021-08-07 22:52:14 +0530

    Refactor tests

--- list/linkedList_test.go
+++ list/linkedList_test.go
@@ -5,16 +5,16 @@ import "testing"
 func TestLinkedList(t *testing.T) {
 	linkedList := &LinkedList{}
 	if linkedList.HeadNode != nil {
-		t.Error("Expected nil")
+		t.Error("Test Failed: {} inputted, {} expected, recieved: {}", nil, nil, linkedList.HeadNode)
 	}
 
 	linkedList.AddToHead(2)
 	if linkedList.HeadNode.Property != 2 {
-		t.Error("Expected 2")
+		t.Error("Test Failed: {} inputted, {} expected, recieved: {}", 2, 2, linkedList.HeadNode.Property)
 	}
 
 	linkedList.AddToHead(3)
 	if linkedList.HeadNode.Property != 3 {
-		t.Error("Expected 3")
+		t.Error("Test Failed: {} inputted, {} expected, recieved: {}", 3, 3, linkedList.HeadNode.Property)
 	}
 }
